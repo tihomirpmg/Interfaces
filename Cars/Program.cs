f218f32a14fa7609a3ef1a2838cfdaed1d41fba6
Тихомир Найденов <tihomirnaydenov02@pmgkk.com>
2021-08-18 13:17:38 +0300

    Interfaces and Abstraction LAB 18/08/21y.

---
+++ Cars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar seat = new Seat("Leon", "Grey");
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+            
+            ICar seat2 = new Seat("Cordoba", "Dark Grey");
+            ICar tesla2 = new Tesla("Model X", "White", 5);
+            
+            Console.WriteLine(seat.ToString());
+
+            Console.WriteLine("==========================");
+
+            Console.WriteLine(tesla.ToString());
+
+            Console.WriteLine("==========================");
+
+            Console.WriteLine(seat2.ToString());
+
+            Console.WriteLine("==========================");
+
+            Console.WriteLine(tesla2.ToString());
+
+            Console.WriteLine("==========================");
+        }
+    }
+}
